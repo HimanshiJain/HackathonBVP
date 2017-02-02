c45704c60e2dbac4a7067192e83108ec29b8b196
Himanshi Jain <himshijain.hj@gmail.com>
2017-02-02 11:44:45 +0530

    Initialise

---
+++ app/src/main/java/com/application/himanshijain/hackathonbvp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.application.himanshijain.hackathonbvp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
